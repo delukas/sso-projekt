6725a6b31ded26411ceba7e3a67cc6ce8c35943a
delukas <lukas.kotlarski@gmail.com>
2025-05-11 22:25:43 +0200

    add token-view + information of user

--- sso-app/src/App.jsx
+++ sso-app/src/App.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { useKeycloak } from '@react-keycloak/web';
+import JsonView from "@uiw/react-json-view";
+import { githubLightTheme } from "@uiw/react-json-view/githubLight";
 
 const App = () => {
-  const { keycloak } = useKeycloak();
+    const { keycloak } = useKeycloak();
 
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>React Keycloak SSO</h1>
-      {keycloak.authenticated ? (
-        <div>
-          <p>Willkommen, {keycloak.tokenParsed?.preferred_username}</p>
-          <button onClick={() => keycloak.logout()}>Abmelden</button>
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>React Keycloak SSO</h1>
+            {keycloak.authenticated ? (
+                <div>
+                    <h2>Willkommen, { keycloak.tokenParsed?.name }</h2>
+                    {keycloak.tokenParsed?.resource_access?.["realm-management"]?.roles.find(value => value === "realm-admin") && (
+                        <>
+                            <p style={{color: "red"}}>Diesen Text können nur Admins sehen.</p>
+                            <br/>
+                        </>
+                    )}
+                    <button onClick={ () => keycloak.logout() }>Abmelden</button>
+                    <br/>
+                    <p>Das ist dein Token:</p>
+                    <br/>
+                    <JsonView value={keycloak.tokenParsed} style={githubLightTheme}/>
+                </div>
+          ) : (
+                <button onClick={ () => keycloak.login() }>Anmelden</button>
+          )}
         </div>
-      ) : (
-        <button onClick={() => keycloak.login()}>Anmelden</button>
-      )}
-    </div>
-  );
+    );
 };
 
 export default App;
