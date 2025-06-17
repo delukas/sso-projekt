46f04aaa007a3bbe8782513f15807a984ca40f7a
Lukas Kotlarski <lukas.kotlarski@gmail.com>
2025-06-17 13:34:01 +0200

    append reload for unauthorized

--- sso-app/src/App.jsx
+++ sso-app/src/App.jsx
@@ -3,8 +3,16 @@ import { useKeycloak } from '@react-keycloak/web';
 import JsonView from "@uiw/react-json-view";
 import { githubLightTheme } from "@uiw/react-json-view/githubLight";
 
+
 const App = () => {
     const { keycloak } = useKeycloak();
+
+
+    if(keycloak.authenticated && !keycloak.tokenParsed?.resource_access?.["sso-app"]?.roles?.find(value => value === "access")){
+        keycloak.logout().then(() => {
+            window.location.reload();
+        })
+    }
 
     return (
         <div style={{ padding: '2rem' }}>
@@ -12,7 +20,7 @@ const App = () => {
             {keycloak.authenticated ? (
                 <div>
                     <h2>Willkommen, { keycloak.tokenParsed?.name }</h2>
-                    {keycloak.tokenParsed?.resource_access?.["realm-management"]?.roles.find(value => value === "realm-admin") && (
+                    {keycloak.tokenParsed?.realm_access?.roles?.find(value => value === "admin") && (
                         <>
                             <p style={{color: "red"}}>Diesen Text können nur Admins sehen.</p>
                             <br/>
