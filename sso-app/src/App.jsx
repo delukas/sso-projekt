c6534ddce4e62905d5440ee5302e2d0d0af71f33
Lukas Kotlarski <lukas.kotlarski@gmail.com>
2025-06-22 22:27:36 +0200

    move role logic

--- sso-app/src/App.jsx
+++ sso-app/src/App.jsx
@@ -7,13 +7,6 @@ import { githubLightTheme } from "@uiw/react-json-view/githubLight";
 const App = () => {
     const { keycloak } = useKeycloak();
 
-
-    if(keycloak.authenticated && !keycloak.tokenParsed?.resource_access?.["sso-app"]?.roles?.find(value => value === "access")){
-        keycloak.logout().then(() => {
-            window.location.reload();
-        })
-    }
-
     return (
         <div style={{ padding: '2rem' }}>
             <h1 style={{fontFamily: "Calibri"}}>React Keycloak SSO</h1>
@@ -27,10 +20,16 @@ const App = () => {
                         </>
                     )}
                     <button style={{fontFamily: "Calibri"}} onClick={ () => keycloak.logout() }>Abmelden</button>
-                    <br/>
-                    <p style={{fontFamily: "Calibri"}}>Das ist dein Token:</p>
-                    <br/>
-                    <JsonView value={keycloak.tokenParsed} style={githubLightTheme}/>
+                    {keycloak.tokenParsed?.resource_access?.["sso-app"]?.roles?.find(value => value === "access") ?
+                        <>
+                            <br/>
+                            <p style={{fontFamily: "Calibri"}}>Das ist dein Token:</p>
+                            <br/>
+                            <JsonView value={keycloak.tokenParsed} style={githubLightTheme}/>
+                        </>
+                        :
+                        <p>Du hast keinen Zugriff.</p>
+                    }
                 </div>
           ) : (
                 <button style={{fontFamily: "Calibri"}} onClick={ () => keycloak.login() }>Anmelden</button>
